473f64d6b60a1e0063330c41f0baa9225dae60fc
unknown <aminsoltani.nia@gmail.com>
2019-10-26 23:57:19 +0330

    angular S

---
+++ src/app/recipies/resipie-detail/resipie-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resipie-detail',
+  templateUrl: './resipie-detail.component.html',
+  styleUrls: ['./resipie-detail.component.css']
+})
+export class ResipieDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
